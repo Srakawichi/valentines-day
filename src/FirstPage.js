c1606490965d8a7dd4fae5cbddb09e8c16923fdd
Srakawichi <nkmr9477@gmail.com>
2025-01-14 20:55:21 +0100

    Serverless Function for FistPage.js

--- src/FirstPage.js
+++ src/FirstPage.js
@@ -3,20 +3,39 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import AnimatedGif from "./AnimatedGif";
 
-
 export default function FirstPage() {
+
+    const handleButtonClick = (button) => {
+        fetch('/.netlify/functions/trackClick', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                element: button,
+                timestamp: new Date().toISOString(),
+            }),
+        });
+    };
+
     return (
         <div className="container">
             <h1>Möchtest du mein Valentins-Date sein?</h1>
             <div className="buttons-container">
                 <Link to="/Activity">
-                    <button className="YesButton">
+                    <button
+                        className="YesButton"
+                        onClick={() => handleButtonClick('Ja, ich will')}
+                    >
                         Ja, ich will
                     </button>
                 </Link>
                 <Link to="/Nope">
-                    <button className="NoButton" >
-                    Ne, hihi
+                    <button
+                        className="NoButton"
+                        onClick={() => handleButtonClick('Ne, hihi')}
+                    >
+                        Ne, hihi
                     </button>
                 </Link>
             </div>
